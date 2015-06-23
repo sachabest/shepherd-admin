847da8c4a9e6d57c110f0292b03213dcfb06ed0f
Sacha Best <sachabest@outlook.com>
2015-06-22 21:03:17 -0700

    README with instructions, some boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # shepherd-admin
 Admin backend for PennMed Shepherd
+
+## Goals
+* Upload data from CSV
+* Review step to confirm data
+* Send to Parse
+
+## Details
+* The review step should look extremely similar to Parse's backend. You should be able to view by object type (of which there are 3-5) and switch between just as you would in the data browser
+* There are three object types: Complaint, Diagnosis, Test, Prescription, and Treatment
+* https://www.parse.com/apps/shepherd--4/collections for more details
+* Objects that do not exist should be created, but not duplicated. That is to say, if two new Complaints are introduced that have pointers to a new Treatment, only one new Treatment should be made. 
+
+## Reach Goals
+* Allow editing of all data once in place (after submission)
---
+++ cloud/app.js
@@ -0,0 +1,59 @@
+
+// These two lines are required to initialize Express in Cloud Code.
+express = require('express');
+app = express();
+
+// Parse info
+var clientKey = '6XbLtHBOgarLeMI7ISWqjBqZfBno6lffUsMxJklP';
+var appId = 'sC51qbtpTGmAuGNXHEQO61uvIYEoC7XClyIuIOb7';
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// Passport.js configuration
+passport = require('passport-parse');
+var parse_passport = new ParseStrategy({
+	appId,
+	clientKey
+});
+passport.use(parse_passport);
+
+passport.authenticate('parse', function(err, user, info) {
+	if (err) {
+		return res.status(400).json({payload : {error: info}, message : info.message});
+    }
+	if (!user) { 
+		return res.status(400).json({payload : {error: info}, message : info.message});
+	}
+	req.logIn(user, function(err) {
+		if (err) {
+			return res.status(400).json({payload : {error: err}, message : info.message});
+		}
+		return res.json({
+			payload : req.user,
+			message : "Authentication successfull"
+		});
+	});
+})(req,res);
+
+// // Example reading from the request query string of an HTTP get request.
+// app.get('/test', function(req, res) {
+//   // GET http://example.parseapp.com/test?message=hello
+//   res.send(req.query.message);
+// });
+
+// // Example reading from the request body of an HTTP post request.
+// app.post('/test', function(req, res) {
+//   // POST http://example.parseapp.com/test (with request body "message=hello")
+//   res.send(req.body.message);
+// });
+
+app.get('/', passport.authenticate('parse'), function(req, res) {
+	// now guaranteed to be authenticated WOO
+	res.render('main', { user_obj: req.user });	
+});
+
+// Attach the Express app to Cloud Code.
+app.listen();
