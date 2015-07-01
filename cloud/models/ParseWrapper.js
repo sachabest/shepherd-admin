d581230b567c862dab73807d3cbf2377a554522f
Vincent Do <vincent.do@10gen.com>
2015-07-01 01:42:36 -0400

    Add function to parse CSV to object

--- cloud/models/ParseWrapper.js
+++ cloud/models/ParseWrapper.js
@@ -6,9 +6,97 @@ var Parse = require('parse').Parse;
 var clientKey = '6XbLtHBOgarLeMI7ISWqjBqZfBno6lffUsMxJklP';
 var appId = 'sC51qbtpTGmAuGNXHEQO61uvIYEoC7XClyIuIOb7';
 
+var COMPLAINT = 1;
+var DIAGNOSIS_STATUS = 2;
+var DIAGNOSIS = 3;
+var DIAGNOSTIC_TEST = 4;
+var PHARMA_TREATMENT = 6;
+var NON_PHARMA_TREATMENT = 8;
+
 Parse.initialize(appId, clientKey);
 
+var Complaint = Parse.Object.extend({
+	className: 'Complaint',
+
+	initialize: function(category, complaint) {
+		this.category = category;
+		this.complaint = complaint;
+		this.diagnoses = [];
+	},
+
+	addDiagnosis: function(diagnosis) {
+		this.diagnoses.push(diagnosis);
+	}
+});
+
+var Diagnosis = Parse.Object.extend({
+	className: 'Diagnosis',
+
+	initialize: function(category, name) {
+		this.category = category;
+		this.name = name;
+	},
+
+	addTreatment: function(treatment) {
+
+	},
+
+	addPrescription: function(prescription) {
+
+	}
+});
+
+var DiagnosticTest = Parse.Object.extend({
+	className: 'DiagnosticTest',
+
+	initialize: function(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+});
+
+var Prescription = Parse.Object.extend({
+	className: 'Prescription',
+
+	initialize: function(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+});
+
+var Treatment = Parse.Object.extend({
+	className: 'Treatment',
+
+	initialize: function(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+});
+
 var recordToParseObject = function(record) {
+
+	// Parse record into objects
+	var complaint = new Complaint(record[0], record[COMPLAINT]);
+	var diagnosis;
+	var dianosticTest;
+	var treatment;
+	var prescription;
+	var parts;
+	if (record[DIAGNOSIS_STATUS] === 'Has diagnosis') {
+		diagnosis = new Diagnosis(record[DIAGNOSIS]);
+		parts = record[PHARMA_TREATMENT].split(': ');
+		prescription = new Prescription(parts[0], parts[1]);
+		parts = record[NON_PHARMA_TREATMENT].split(': ');
+		treatment = new Treatment(parts[0], parts[1]);
+	} else {
+		parts = record[DIAGNOSTIC_TEST].split(': ');
+		dianosticTest = new DiagnosticTest(parts[0], parts[1]);
+	}
+	
+	// Creating relationships
+	if (diagnosis) {
+		diagnosis.addTreatment();
+	}
 
 };
 
--- cloud/routes/routes.js
+++ cloud/routes/routes.js
@@ -4,10 +4,74 @@ var CSVParser = require('csv-parse');
 var Parse = require('../models/ParseWrapper');
 var fs = require('fs');
 
+var COMPLAINT_CATEGORY = 0;
+var COMPLAINT_NAME = 1;
+var DIAGNOSIS_STATUS = 2;
+var DIAGNOSTIC_TEST = 3;
+var DIAGNOSIS = 4;
+var PHARMACOTHERAPY = 5;
+var NON_PHARMACOTHERAPY = 6;
+
+var recordToObject = function(record) {
+    var parts;
+    var complaint = {
+        name: record[COMPLAINT_NAME],
+        category: record[COMPLAINT_CATEGORY]
+    };
+    var diagnosis = {
+        name: record[DIAGNOSIS]
+    };
+
+    var test;
+    var prescription;
+    var treatment;
+
+    if (record[DIAGNOSTIC_TEST]) {
+        parts = record[DIAGNOSTIC_TEST].split(': ');
+        test = {
+            name: parts[0],
+            price: parts[1]
+        };
+    }
+    
+    if (record[PHARMACOTHERAPY]) {
+        parts = record[PHARMACOTHERAPY].split(': ');
+        prescription = {
+            name: parts[0],
+            price: parts[1]
+        };
+
+        treatment = {
+            category: 'Pharmacotherapy',
+            name: parts[0],
+            price: parts[1]
+        };
+    }
+
+    if (record[NON_PHARMACOTHERAPY]) {
+        parts = record[NON_PHARMACOTHERAPY].split(': ');
+        treatment = {
+            category: 'Non-Pharmacotherapy',
+            name: parts[0],
+            price: parts[1],
+        };
+    }
+
+    var result = {
+        complaint: complaint,
+        diagnosis: diagnosis,
+        test: test,
+        prescription: prescription,
+        treatment: treatment
+    };
+
+    return result;
+};
+
 var processCSVFile = function(srcFile, columns, onNewRecord, errorHandler, done) {
 	var source = fs.createReadStream(srcFile);
 
-    var linesRead = 0;
+    var collection = [];
 
     var parser = new CSVParser({
         delimiter: ',', 
@@ -16,8 +80,7 @@ var processCSVFile = function(srcFile, columns, onNewRecord, errorHandler, done)
     parser.on("readable", function(){
         var record;
         while ((record = parser.read())) {
-            onNewRecord(record);
-            linesRead += 1;
+            collection.push(onNewRecord(record));
         }
     });
 
@@ -25,8 +88,9 @@ var processCSVFile = function(srcFile, columns, onNewRecord, errorHandler, done)
         errorHandler(error);
     });
 
-    parser.on("end", function(){
-        done(linesRead);
+    parser.on("end", function() {
+        console.log(collection);
+        done('Parsed all records');
     });
 
     source.pipe(parser);
@@ -34,11 +98,6 @@ var processCSVFile = function(srcFile, columns, onNewRecord, errorHandler, done)
 
 var readCSVFile = function(req, res, next) {
 	var filePath = req.files['csv-data'].path;
-
-    // Replacing this with actual parsing function
-    function onNewRecord(record){
-        console.log(record);
-    }
 
     function onError(error){
         console.log(error);
@@ -49,7 +108,7 @@ var readCSVFile = function(req, res, next) {
     }
 
     var columns = true; 
-    processCSVFile(filePath, columns, onNewRecord, onError, done);
+    processCSVFile(filePath, columns, recordToObject, onError, done);
 };
 
 var routes = {
