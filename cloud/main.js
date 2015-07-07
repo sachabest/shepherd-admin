6e96e7ad7bc6d7769011c585a6b4e849328a2a1b
Vincent Do <vincent.do@10gen.com>
2015-07-07 01:20:28 -0400

    cloud code debug

--- cloud/main.js
+++ cloud/main.js
@@ -5,25 +5,130 @@ var _ = require('underscore');
 Parse.Cloud.beforeSave('Complaint', function(request, response) {
     var Complaint = Parse.Object.extend('Complaint');
     var query = new Parse.Query(Complaint);
-    query.equalTo('name', request.object.get('name'));
+    query.equalTo('Name', request.object.get('Name'));
     query.first()
     	.then(function(object) {
     		if (object) {
-		        var diagnoses = object.get('diagnoses');
+		        var diagnoses = object.get('Diagnoses');
 		        diagnoses = _.map(diagnoses, function(diagnosis) {
-					return diagnosis.get('name');
+					return diagnosis.get('Name');
 		        });
 
-		        var exist = _.contains(diagnoses, request.object.get('diagnosis').get('name'));
+		        var exist = _.contains(diagnoses, request.object.get('Diagnoses').get('Name'));
 		        if (!exist) {
-			      	object.add('diagnoses', request.object.get('diagnosis'));
-		        	response.success();
+			      	object.add('Diagnoses', request.object.get('Diagnosis'));
+			      	return object.save();
+		        	// object.save();
+		        	// 	.then(function() {
+		        	// 		response.error('Object existed. Added new diagnosis');
+		        	// 	}, function() {
+		        	// 		response.error('Objected existed and failed to add new diagnosis');
+		        	// 	});
+		        	// response.error();
 		        }
 		        else {
-		        	response.error('Object already exist');
+		        	return Parse.Promise.as(false);
 		        }
 		    }
-    	});
+    	}).then(function(object) {
+    		if (object) {
+    			response.error('Object existed');
+    		} else {
+    			response.success();
+    		}
+    	},  function(error) {
+    		response.error('Error performing save');
+    	})
+
 });
 
 
+Parse.Cloud.beforeSave('Prescription', function(request, response) {
+	var Prescription = Parse.Object.extend('Prescription');
+	var query = new Parse.Query(Prescription);
+	query.equalTo('Name', request.object.get('Name'));
+	query.first()
+		.then(function(object) {
+			response.error('Object already exist');
+		},
+		function() {
+			response.success();
+		});
+});
+
+
+Parse.Cloud.beforeSave('Diagnosis', function(request, response) {
+	var Diagnosis = Parse.Object.extend('Diagnosis');
+	var Complaint = Parse.Object.extend('Complaint');
+	var query = new Parse.Query(Diagnosis);
+	query.equalTo('Name', request.object.get('Name'));
+	query.first()
+		.then(function(diagnosis) {
+			response.error('Object already exist');
+		}, function() {
+			var complaintQuery = new Parse.Query(Complaint);
+			complaintQuery.equalTo('Name', request.object.get('Complaint').get('Name'));
+			complaintQuery.first()
+				.then(function(complaint) {
+					request.object.set('Complaint', complaint);
+					response.success();
+				}, function() {
+					response.success();
+				})
+		});
+});
+
+Parse.Cloud.beforeSave('Test', function(request, response) {
+	var Test = Parse.Object.extend('Test');
+	var Complaint = Parse.Object.extend('Complaint');
+	var query = new Parse.Query(Test);
+	query.equalTo('Name', request.object.get('Name'));
+	query.first()
+		.then(function(test) {
+			response.error('Object already exist');
+		}, function() {
+			var complaintQuery = new Parse.Query(Complaint);
+			complaintQuery.equalTo('Name', request.object.get('Complaint').get('Name'));
+			complaintQuery.first()
+				.then(function(complaint) {
+					request.object.set('Complaint', complaint);
+					response.success();
+				}, function() {
+					response.success();
+				});
+		});
+});
+
+Parse.Cloud.beforeSave('Treatment', function(request, response) {
+	var Treatment = Parse.Object.extend('Treatment');
+	var Diagnosis = Parse.Object.extend('Diagnosis');
+	var Prescription = Parse.Object.extend('Prescription');
+	var query = new Parse.Query(Treatment);
+
+	var updatePrescription = function() {
+		var prescriptionQuery = new Parse.Query(Prescription);
+		prescriptionQuery.equalTo('Name', request.object.get('Prescription').get('Name'));
+		prescriptionQuery.first()
+			.then(function(prescription) {
+				request.object.set('Prescription', prescription);
+				response.success();
+			}, function() {
+				response.success();
+			});
+	};
+	query.equalTo('Name', request.object.get('Name'));
+	query.first()
+		.then(function(test) {
+			response.error('Object already exist');
+		}, function() {
+			var diagnosisQuery = new Parse.Query(Diagnosis);
+			diagnosisQuery.equalTo('Name', request.object.get('Diagnosis').get('Name'));
+			diagnosisQuery.first()
+				.then(function(diagnosis) {
+					request.object.set('Diagnosis', diagnosis);
+					updatePrescription();
+				}, function() {
+					updatePrescription();
+				});
+		})
+});
--- server/models/ParseWrapper.js
+++ server/models/ParseWrapper.js
@@ -13,27 +13,33 @@ var Complaint = Parse.Object.extend({
 	className: 'Complaint',
 
 	initialize: function(opts) {
-		this.category = opts.category;
-		this.complaint = opts.complaint;
-		this.diagnoses = [];
+		this.Category = opts.category;
+		this.Name = opts.name;
+		this.Diagnoses = [opts.diagnosis];
 	}
 });
 
 var Diagnosis = Parse.Object.extend({
 	className: 'Diagnosis',
 
-	initialize: function(opts) {
-		this.category = opts.category;
-		this.name = opts.name;
+	initialize: function(opts, complaintReference) {
+		this.Category = opts.category;
+		this.Name = opts.name;
+		if (complaintReference) {
+			this.Complaint = complaintReference;
+		}
 	}
 });
 
 var Test = Parse.Object.extend({
 	className: 'Test',
 
-	initialize: function(opts) {
-		this.name = opts.name;
-		this.price = opts.price;
+	initialize: function(opts, complaintReference) {
+		this.Name = opts.name;
+		this.Price = opts.price;
+		if (complaintReference) {
+			this.Complaint = complaintReference;
+		}
 	}
 });
 
@@ -41,18 +47,24 @@ var Prescription = Parse.Object.extend({
 	className: 'Prescription',
 
 	initialize: function(opts) {
-		this.name = opts.name;
-		this.price = opts.price;
+		this.Name = opts.name;
+		this.Price = opts.price;
 	}
 });
 
 var Treatment = Parse.Object.extend({
 	className: 'Treatment',
 
-	initialize: function(opts) {
-		this.category = opts.category;
-		this.name = opts.name;
-		this.price = opts.price;
+	initialize: function(opts, diagnosisReference, prescriptionReference) {
+		this.Category = opts.category;
+		this.Name = opts.name;
+		this.Price = opts.price;
+		if (diagnosisReference) {
+			this.Diagnosis = diagnosisReference;
+		}
+		if (prescriptionReference) {
+			this.Prescription = prescriptionReference;
+		}
 	}
 });
 
@@ -60,18 +72,84 @@ var objectToParseObject = function(objects) {
 	var complaints = _.map(objects, function(object){
 		return new Complaint(object.complaint);
 	});
-	// var complaint = new Complaint(object.complaint);
-	// var diagnosis = new Diagnosis(object.diagnosis);
-	// var test = new Test(object.test);
-	// var treatment = new Treatment(object.treatment);
-	// var prescription = new Prescription(object.prescription);
+
+	var diagnoses = _.map(objects, function(object) {
+		var complaint = _.find(complaints, function(compl) {
+			if (compl.Name === object.diagnosis.complaint) {
+				return compl;
+			}
+		});
+		return new Diagnosis(object.diagnosis, complaint);
+	});
+
+	var tests = _.map(objects, function(object) {
+		var complaint = _.find(complaints, function(compl) {
+			if (compl.Name === object.test.complaint) {
+				return compl;
+			}
+		});
+		return new Test(object.test, complaint);
+	});
+
+	var prescriptions = _.map(objects, function(object) {
+		return new Prescription(object.prescription);
+	});
+
+	var treatments = _.map(objects, function(object) {
+		var diagnosis = _.find(diagnoses, function(diag) {
+			if (diag.Name === object.treatment.diagnosis) {
+				return diag;
+			}
+		});
+		var prescription;
+		if (object.treatment.category === 'Pharmacotherapy') {
+			prescription = _.find(prescriptions, function(pres) {
+				if (pres.name === object.treatment.name) {
+					return pres;
+				}
+			});
+		}
+		return new Treatment(object.treatment, diagnosis, prescription);
+	});
+
+	// console.log(complaints);
+	// console.log(treatments);
 
 	// Save objects
-	return Parse.Object.saveAll(complaints);
-	// diagnosis.save();
-	// test.save();
-	// treatment.save();
-	// prescription.save();
+
+	var complaintsPromise;
+	var prescriptionPromise;
+	var diagnosesPromise;
+	var testsPromise;
+	var treatmentsPromise;
+
+
+	// var parsePromise = Parse.Object.saveAll(complaints)
+	// 	.then(function() {
+	// 		return Parse.Object.saveAll(prescriptions);
+	// 	}, function(error) {
+	// 		console.log(error);
+	// 		return error;
+	// 	})
+	// 	.then(function() {
+	// 		return Parse.Object.saveAll(diagnoses);
+	// 	}, function(error) {
+	// 		return Parse.Promise.error(error);
+	// 	})
+	// 	.then(function() {
+	// 		return Parse.Object.saveAll(tests);
+	// 	}, function(error) {
+	// 		return Parse.Promise.error(error);
+	// 	})
+	// 	.then(function() {
+	// 		return Parse.Object.saveAll(treatments);
+	// 	}, function(error) {
+	// 		return Parse.Promise.error(error);
+	// 	});
+
+	var parsePromise = Parse.Object.saveAll(complaints);
+	
+	return parsePromise;
 };
 
 var ParseWrapper = {
